0252865d94777bb99e6b125d4bb9d113b0b8d58b
sampremm <samprem888111@gmail.com>
2024-11-01 20:09:03 +0530

    updated .env
    updated .env - fixed  the issue

--- blogging website - frontend/src/common/firebase.jsx
+++ blogging website - frontend/src/common/firebase.jsx
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLru5RwPMKcOPtaduRSh5crOBiM0tAFEo",
-  authDomain: "react-js-blog-website-c9a24.firebaseapp.com",
-  projectId: "react-js-blog-website-c9a24",
-  storageBucket: "react-js-blog-website-c9a24.appspot.com",
-  messagingSenderId: "118203446965",
-  appId: "1:118203446965:web:a1f9add898121e10eef0fc"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 
 const app = initializeApp(firebaseConfig);
